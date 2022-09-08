764528dabe146e367ca20ae31ac88c22f2043d18
Salihan04 <salihan04@gmail.com>
2022-09-08 15:30:34 +0800

    Add resources to resolve tfsec findings

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,3 +1,27 @@
+# tfsec:ignore:aws-s3-enable-bucket-logging tfsec:ignore:aws-s3-enable-versioning
 resource "aws_s3_bucket" "test_bucket" {
   bucket = var.bucket_name
 }
+
+# resource "aws_s3_bucket_public_access_block" "test_bucket" {
+#   bucket                  = aws_s3_bucket.test_bucket.id
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+#   block_public_acls       = true
+# }
+
+# resource "aws_kms_key" "test" {
+#   enable_key_rotation = true
+# }
+
+# resource "aws_s3_bucket_server_side_encryption_configuration" "test_bucket" {
+#   bucket = aws_s3_bucket.test_bucket.bucket
+
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       kms_master_key_id = aws_kms_key.test.arn
+#       sse_algorithm     = "aws:kms"
+#     }
+#   }
+# }
