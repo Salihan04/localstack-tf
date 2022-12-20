a9bcaeefe017ac090c6f999d6b7b3deeb66e2bd3
Salihan04 <salihan04@gmail.com>
2022-12-20 13:31:40 +0800

    Add tests with Terratest

---
+++ examples/local_s3/local_s3_test.go
@@ -0,0 +1,29 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+// An example of how to test the Terraform module in examples/terraform-aws-s3-example using Terratest.
+func TestTerraformAwsS3Example(t *testing.T) {
+	t.Parallel()
+
+	// Construct the terraform options with default retryable errors to handle the most common retryable errors in
+	// terraform testing.
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		// The path to where our Terraform code is located
+		TerraformDir: ".",
+	})
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terraformOptions)
+
+	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
+	terraform.InitAndApply(t, terraformOptions)
+
+	bucketArn := terraform.Output(t, terraformOptions, "arn")
+	assert.Equal(t, "arn:aws:s3:::localstack-bucket", bucketArn)
+}
